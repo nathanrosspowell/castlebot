adc145730c3b63fcb3618137a1a026ff557bae65
Nathan Ross Powell <nathanrosspowell@gmail.com>
2016-03-13 03:02:22 -0400

    Polling
    Check for latest HTML changes.
    Check against DB
    Add new records
    Ping out message with new data

--- data/databaseGenerator.js
+++ data/databaseGenerator.js
@@ -1,16 +1,13 @@
 'use strict';
 
 /**
- * Command line script that generates a SQLite database file that contains jokes about Castle Bot using the
- * wonderful http://api.icndb.com/jokes APIs
+ * Command line script that generates a SQLite database file that contains tables for GoFundMe data.
  *
  * Usage:
  *
  *   node databaseGenerator.js [destFile]
  *
  *   destFile is optional and it will default to "castlebot.db"
- *
- * @author Luciano Mammino <lucianomammino@gmail.com>
  */
 
 var path = require('path');
@@ -18,80 +15,35 @@ var request = require('request');
 var Async = require('async');
 var ProgressBar = require('progress');
 var sqlite3 = require('sqlite3').verbose();
+var GoFundMe = require('./../lib/gofundme');
 
 var outputFile = process.argv[2] || path.resolve(__dirname, 'castlebot.db');
 var db = new sqlite3.Database(outputFile);
 
-// executes an API request to count all the available jokes
-request('http://api.icndb.com/jokes/count', function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-        var count = JSON.parse(body).value;
-        var savedJokes = 0;
-        var index = 0;
-        var bar = new ProgressBar(':bar :current/:total', {total: count});
-
-        // Prepares the database connection in serialized mode
-        db.serialize();
-        // Creates the database structure
-        db.run('CREATE TABLE IF NOT EXISTS info (name TEXT PRIMARY KEY, val TEXT DEFAULT NULL)');
-        db.run('CREATE TABLE IF NOT EXISTS jokes (id INTEGER PRIMARY KEY, joke TEXT, used INTEGER DEFAULT 0)');
-        db.run('CREATE INDEX jokes_used_idx ON jokes (used)');
-
-        // The idea from now on is to iterate through all the possible jokes starting from the index 1 until we can
-        // find all the available ones. There might be holes in the sequence, so we might want to issue all the request
-        // sequentially and count the successful requests until we get the total amount of jokes.
-        // We are going to use the function Async.whilst so we need to define 3 functions: test, task and onComplete
-
-        // Tests whether to stop fetching jokes. It gets called before starting a new iteration
-        var test = function () {
-            return savedJokes < count;
-        };
-
-        // The task executed at every iteration. Basically fetches a new joke and creates a new record in the database.
-        var task = function (cb) {
-            request('http://api.icndb.com/jokes/' + (++index) + '?escape=javascript', function (err, response, body) {
-                // handle possible request errors by stopping the whole process
-                if (err || response.statusCode !== 200) {
-                    console.log(index, error, response.statusCode);
-
-                    return cb(error || response.statusCode);
-                }
-
-                // invalid ids generates an invalid JSON response (basically an HTML output), so we can
-                // check for it by detecting JSON parse errors and skip the id by calling the callback completion
-                // function for the current iteration
-                var result = null;
-                try {
-                    result = JSON.parse(body).value;
-                } catch (ex) {
-                    return cb(null);
-                }
-
-                db.run('INSERT INTO jokes (joke) VALUES (?)', result.joke, function (err) {
-                    if (err) {
-                        return cb(err);
-                    }
-
-                    ++savedJokes;
-                    bar.tick();
-                    return cb(null);
-                });
-            });
-        };
-
-        // On completion we just need to show errors in case we had any and close the database connection
-        var onComplete = function (err) {
-            db.close();
+// Prepares the database connection in serialized mode
+db.serialize();
+// Creates the database structure
+db.run('CREATE TABLE info (name TEXT PRIMARY KEY, val TEXT DEFAULT NULL)');
+db.run('CREATE TABLE campaign (name TEXT PRIMARY KEY, val TEXT DEFAULT NULL)');
+db.run('CREATE TABLE donors (id INTEGER PRIMARY KEY, donation_id INTEGER, name TEXT, amount INTEGER, message TEXT)');
+// Get the GoFundMe data.
+var input = {
+    //url: 'https://www.gofundme.com/39t6wr3c'
+    url: 'https://www.gofundme.com/tprseancullen'
+}
+console.log("GoFundMe: " + input.url);
+GoFundMe(input, function(data){
+    console.log("Adding", data.donations, "records...");
+    for ( var i in data.donors ) {
+        var d = data.donors[i];
+            console.log(i, d);
+        db.run('INSERT INTO donors (donation_id, name, amount, message) VALUES (?,?,?,?)',
+                [i, d.name, d.amount, d.message],
+                function (err) {
             if (err) {
-                console.log('Error: ', err);
-                process.exit(1);
+                console.log("ERR:", err);
             }
-        };
-
-        // triggers the asynchronous iteration using the previously defined test, task and onComplete functions
-        return Async.whilst(test, task, onComplete);
+        });
     }
-
-    console.log('Error: unable to count the total number of jokes');
-    process.exit(1);
+    db.close();
 });
